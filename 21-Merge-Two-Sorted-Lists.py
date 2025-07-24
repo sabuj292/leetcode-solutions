f9e8e133155df21d7aa0bc427cc9c05bc6d178c4
Shahriar Mahmud Sabuj <shahriarmahmud292@gmail.com>
2025-07-24 16:12:58 +0600

    added better version in merger two sorted lists

--- 21-Merge-Two-Sorted-Lists.py
+++ 21-Merge-Two-Sorted-Lists.py
@@ -57,3 +57,56 @@ if __name__ == "__main__":
     merged = solution.mergeTwoLists(list1, list2)
 
     print("Merged List:", linked_list_to_list(merged))
+    
+    
+    
+# another way with better run time
+
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+         
+# no need the following "addNode function"
+def addNode(head, added):
+    while(head.next!=None):
+        head = head.next
+    head.next = added
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not list1:
+            return list2
+        if not list2:
+            return list1
+        #set head and tail node
+        if list1.val>list2.val:
+            head=tail=list2
+            list2 = list2.next
+        else:
+            head=tail=list1
+            list1 = list1.next
+        while(list1 and list2):
+            if(list1.val>list2.val):
+                tail.next=list2
+                tail=list2
+                list2=list2.next
+            else:
+                tail.next=list1
+                tail=list1
+                list1=list1.next
+        #attach leftover nodes        
+        if list1:
+            tail.next = list1
+        else:
+            tail.next=list2
+        return head
+        
+        #loop until one or both lists are empty(.next is null), add the rest of the remaining array onto the end
+        #
+        
